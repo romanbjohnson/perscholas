ed427703ce3f184e83524e6c7780e5e8484f2096
romanbjohnson <106290601+romanbjohnson@users.noreply.github.com>
2023-01-23 21:22:07 -0500

    Add files via upload

---
+++ Javascript data types.js
@@ -0,0 +1,195 @@
+//Roman Johnson 
+// January 20, 2023
+// Per Scholas Software Engineering Course
+// Javascript Data Types Homework 
+
+// Javascript data types: 
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+// The object data type can contain:
+// 1. An object
+// 2. An array
+// 3. A date
+// Source: 
+
+
+
+// for the homework write 10 examples of each data types (e.g., objects, arrays, integers, strings, booleans (e.g, true/ falese)) for monday 
+// build a project as well that is due on monday
+
+//Strings 
+
+
+let apple = "red" 
+let banana = "yellow" 
+let orange = "orange" 
+let kiwi = "lightbrown"
+let grapes = "purple"
+let zucchini = "darkgreen"
+let pear = "lightgreen"
+let watermelon = "greenstripes"
+let mango = "lightorange"
+let acai = "purpleandgreen"
+
+//Numbers
+
+let base = 12
+let area = 32
+let circumference = 8
+let height = 5.8
+let weight = 200 
+let width = 8 
+let pounds = 160 
+let amountofpencils = 50 
+let amountofshoes = 15 
+let numberpets = 1 
+let ageofcat = 3
+let numberofcars = 0
+
+//Bigint = Big integer 
+
+let bigint
+
+const bigint = Bigint(10)
+console.log(bigint)
+const bigint = "10n"
+const bigint = Bigint(true)
+// result should be 1
+const bigint = Bigint(false)
+// result should be 0 
+const bigint = Bigint(7n)
+const bigint = Bigint (6n)
+const bigint = Bigint (8n)
+const bigint = Bigint (9n)
+const bigint = "5n"
+const bigint = "3n"
+const bigint = "2n"
+
+
+// Booleans 
+
+let x = 2 
+let y = 2 
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+let x = 4 
+let y = 4
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+
+let x = 5
+let y = 5
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+
+let x = 6
+let y = 6 
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+
+let x = 7 
+let y = 7
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+
+let x = 8 
+let y = 8
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+
+let x = 9 
+let y = 9 
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+
+let x = 10
+let y = 10 
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+
+let x = 11 
+let y = 11 
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+
+let x = 12 
+let y = 12 
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+
+let x = 13 
+let y = 13
+let z = 3 
+(x == y) // returns true
+(x == z) // returns false 
+
+
+// Undefined 
+
+
+
+// arrays
+
+const cars = ["Saab", "Volvo", "BMW"];
+const movies = ["friday", "powerranges", "hillshaveeyes"];
+const meats = ["turkey", "beef", "chicken"];
+const cheese = ["american", "pepperjack", "provolone"];
+const names = ["billy", "eric", "charlie"];
+const occupations = ["dentist", "engineer", "teacher"];
+const religions = ["christianity", "islam", "judiasm"];
+const cities = ["memphis", "nashville", "chicago"];
+const capitols = ["nashville", "springfield", "houston"];
+const tea = ["lavender", "earlgrey", "tulsi"];
+
+// 
+
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+
+
+
